cf6c287309f58305e31be20fe8107b436d518e04
retg96 <49927295+retg96@users.noreply.github.com>
2020-07-09 21:29:20 -0500

    logout y idtoken

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email: string;
+  password: string;
+  constructor(private menu: MenuController, private authService: AuthService, public router: Router) {}
 
-  constructor() {}
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  async logOut()
+  {
+    await this.authService.logout();
+    await this.router.navigate(['/home']);
+ 
+  }
 
 }
